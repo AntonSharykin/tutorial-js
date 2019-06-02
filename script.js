d57c09f4c1798e745b5297461f1dd11e71747f2e
Antonio <anton.valianos@gmail.com>
2019-06-02 17:11:40 +0300

    functions

--- script.js
+++ script.js
@@ -1,19 +1,79 @@
-var money = prompt("Till month","0");
-var time = prompt("Date YYYY-MM-DD");
+'use strict'; 
+
+let money,time;
 let appData = {
-budjet : money,
-timeData : time,
-expenses : {},
-optionalExpenses:{},
-income:[],
-savings : false
+    budjet : money,
+    timeData : time,
+    expenses : {},
+    optionalExpenses:{},
+    income:[],
+    savings : true
+    }
+
+function start(){
+    money = +prompt("Till month: ");
+    time = prompt("Date YYYY-MM-DD");
+    while(isNaN(money) || money=="" || money==null){
+    money = +prompt("Till month",'');
+    }
+    
 }
 
-let tempStrForName1=prompt("Name of minuses"),
-mSum1=prompt("sum"),
-tempStrForName2=prompt("Name of minuses"),
-mSum2 = prompt("sum");
-appData.expenses.tempStrForName1=mSum1;
-appData.expenses.tempStrForName2=mSum2;
+function detectBudjet(){
+    appData.budjet=money;
+    appData.moneyPerDay=(appData.budjet/30).toFixed(1);
+    alert("Ежедневный бюджет" +appData.moneyPerDay);
+}
 
-alert("1 day "+appData.money/30);
+function choseExpenses(){
+    for(let i=0;i<2;i++){
+        let tempStrForName=prompt("Name of minuses"),
+        mSum=prompt("sum",0);
+        if((typeof(tempStrForName))==='string' && (typeof(tempStrForName)!=null)){
+        appData.expenses[tempStrForName]=mSum;
+    }
+    }
+}
+
+function choseOptExpenses(){
+    for(let i=0;i<3;i++){
+       let mSum=+prompt("Optional Exp sum");
+       let counter=1;
+       if((mSum!=null) && (mSum!="")){ 
+        appData.optionalExpenses[counter]=mSum;
+        counter++;
+       }
+    }
+}
+
+function checkSavings(){
+    if(appData.savings== true){
+        let save = +prompt("how much did you save?","0"),
+        percent=+prompt("what percent do you have?");
+        appData.monthIncome = save/100/12*percent;
+        alert("Month incoming: "+appData.monthIncome);
+    }
+}
+
+function detectLevel(){
+    if(appData.moneyPerDay<=500) {
+        console.log("Low level");
+    } else if((appData.moneyPerDay)>500 && (appData.moneyPerDay)<5000){
+        console.log("Medium level");
+    }else if((appData.moneyPerDay)>5000){
+        console.log("High level");
+    }
+}
+
+start();
+detectBudjet();
+detectLevel();
+choseExpenses();
+choseOptExpenses();
+checkSavings();
+
+
+
+
+
+
