185c0b04064ef5e89273b07984255d0186ff21c8
Antonio <anton.valianos@gmail.com>
2019-06-09 14:38:56 +0300

    timer

--- js.js
+++ js.js
@@ -1,3 +1,37 @@
-function mes(text){
-    alert(text);
+'use strict';
+let deadline= '2019-06-10';
+
+function getTimeRemaining(endTime){
+    let t=Date.parse(endTime)-Date.parse(new Date()),
+    seconds=Math.floor((t/1000)%60),
+    minutes=Math.floor(((t/1000)/60)%60),
+    hours=Math.floor((t/(1000*60*60)));
+    return {
+        'total':t,
+        'hours':hours,
+        'minutes':minutes,
+        'seconds':seconds
+    };
+    //days=Math.floor((t/(1000*60*60*24)));
 }
+
+function setClock(id,endTime){
+    let timer = document.getElementById('timer'),
+    hours = timer.querySelector('.hours'),
+    minutes=timer.querySelector('.minutes'),
+    seconds=timer.querySelector('.seconds'),
+    timeInterval=setInterval(updateClock,1000); 
+    function updateClock(){
+        let t = getTimeRemaining(endTime);
+        hours.textContent=t.hours;
+        minutes.textContent=t.minutes;
+        seconds.textContent=t.seconds;
+        if(t.total<=0) {
+            clearInterval();
+        }
+    }
+}
+
+setClock('timer',deadline);
+
+
